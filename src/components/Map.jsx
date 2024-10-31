0fa9a22b37ce042a764f06d787b8b231b708a86c
Jens Poulsen <jpoulsen@iworq.com>
2024-10-31 11:10:14 -0600

    add some Navigation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,31 @@
+import { useSearchParams, useNavigate } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const navigate = useNavigate();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
+  return (
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate('form');
+      }}
+    >
+      <h1>Map</h1>
+      <h2>
+        Position: {lat}, {lng}
+      </h2>
+      <button
+        onClick={() => setSearchParams({ lat: '40.7128', lng: '-74.0060' })}
+      >
+        Change position
+      </button>
+    </div>
+  );
 }
 
 export default Map;
